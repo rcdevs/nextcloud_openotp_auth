8e828333a907539fe517e9760d1fb26672b54348
julien RICHARD <julien.richard@rcdevs.com>
2018-09-18 16:36:13 +0200

    Add new Listener - StateChanged classes

---
+++ lib/Listener/StateChangeRegistryUpdater.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * Nexcloud - RCDevs OpenOTP Two-factor Authentication
+ *
+ * @package twofactor_rcdevsopenotp
+ * @author Julien RICHARD
+ * @copyright 2018 RCDEVS info@rcdevs.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Displays <a href="http://opensource.org/licenses/AGPL-3.0">GNU AFFERO GENERAL PUBLIC LICENSE</a>
+ * @license http://opensource.org/licenses/AGPL-3.0 GNU AFFERO GENERAL PUBLIC LICENSE
+ *
+ */
+
+namespace OCA\TwoFactor_RCDevsOpenOTP\Listener;
+
+use OCA\TwoFactor_RCDevsOpenOTP\Event\StateChanged;
+use OCA\TwoFactor_RCDevsOpenOTP\Provider\TwoFactorRCDevsOpenOTPProvider;
+use OCP\Authentication\TwoFactorAuth\IRegistry;
+use Symfony\Component\EventDispatcher\Event;
+
+class StateChangeRegistryUpdater implements IListener {
+
+	/** @var IRegistry */
+	private $registry;
+
+	/** @var TotpProvider */
+	private $provider;
+
+	public function __construct(IRegistry $registry, TwoFactorRCDevsOpenOTPProvider $provider) {
+		$this->registry = $registry;
+		$this->provider = $provider;
+	}
+
+	public function handle(Event $event) {
+		if ($event instanceof StateChanged) {
+			if ($event->isEnabled()) {
+				$this->registry->enableProviderFor($this->provider, $event->getUser());
+			} else {
+				$this->registry->disableProviderFor($this->provider, $event->getUser());
+			}
+		}
+	}
+}
