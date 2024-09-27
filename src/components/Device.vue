da10c8763a60aa9ea910792a8d0c013e5bd90de3
eric <eric.james@bayssette.fr>
2024-09-27 11:45:56 +0200

    Missing some files on commit, mishandling sorry. No impact on released version 1.30.0; only for source code.

---
+++ src/components/Device.vue
@@ -0,0 +1,123 @@
+<!--
+ *
+ * @copyright Copyright (c) 2024, RCDevs (info@rcdevs.com)
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+  -->
+
+<template>
+	<div class="webauthn-device" :data-webauthn-id="id">
+		<span class="icon-webauthn-device" :class="{ disabled: !active }" />
+		{{ name || t('openotp_auth', 'Unnamed key') }}
+		<Actions>
+			<ActionText v-if="createdAt" :title="t('openotp_auth', 'Registered')">
+				<template #icon>
+					<InformationOutline :size="20" />
+				</template>
+				{{ createdAtFormatted }}
+			</ActionText>
+			<ActionCheckbox :checked="active" @update:checked="changeActivation">
+				{{ t('openotp_auth', 'Active') }}
+			</ActionCheckbox>
+			<ActionButton icon="icon-delete" :close-after-click="true" @click="onDelete">
+				{{ t('openotp_auth', 'Remove') }}
+			</ActionButton>
+		</Actions>
+	</div>
+</template>
+
+<script>
+import {
+	NcActions as Actions,
+	NcActionButton as ActionButton,
+	NcActionCheckbox as ActionCheckbox,
+	NcActionText as ActionText,
+} from '@nextcloud/vue'
+import InformationOutline from 'vue-material-design-icons/InformationOutline.vue'
+import { confirmPassword } from '@nextcloud/password-confirmation'
+import moment from '@nextcloud/moment'
+
+export default {
+	name: 'Device',
+	components: {
+		Actions,
+		ActionButton,
+		ActionCheckbox,
+		ActionText,
+		InformationOutline,
+	},
+	props: {
+		entityId: Number,
+		id: String,
+		name: String,
+		active: Boolean,
+		createdAt: {
+			type: Number,
+			default: undefined,
+		},
+	},
+	computed: {
+		createdAtFormatted() {
+			if (!this.createdAt) {
+				return
+			}
+
+			return moment(this.createdAt * 1000).format('L')
+		},
+	},
+	methods: {
+		async onDelete() {
+			try {
+				await confirmPassword()
+				await this.$store.dispatch('removeDevice', this.entityId)
+			} catch (e) {
+				console.error('could not delete device', e)
+			}
+		},
+		async changeActivation(active) {
+			try {
+				await confirmPassword()
+				await this.$store.dispatch('changeActivationState', {
+					entityId: this.entityId,
+					active,
+				})
+			} catch (e) {
+				console.error('could not change device state', e)
+			}
+		},
+	},
+}
+</script>
+
+<style scoped>
+	.webauthn-device {
+		line-height: 300%;
+		display: flex;
+	}
+
+	.device__created_at {
+		color: var(--color-text-maxcontrast);
+	}
+
+	.icon-webauthn-device {
+		display: inline-block;
+		background-size: 100%;
+		padding: 3px;
+		margin: 3px;
+		filter: var(--background-invert-if-dark);
+	}
+</style>
